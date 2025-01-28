17114673f9ee22185a22e5aa878a90031b27d7f1
aldovnlv <aldovnlv@gmail.com>
2025-01-28 12:54:06 -0600

    Update linear.py

--- linear.py
+++ linear.py
@@ -12,7 +12,7 @@ print(tf.__version__)
 X = np.arange(-10.0, 10.0, 1e-2)
 print(X)
 np.random.shuffle(X)
-y =  2.0 * X + 1.0
+y =  3.141592654 * X + 3.141592654
 print(y)
 
 train_end = int(0.6 * len(X))
